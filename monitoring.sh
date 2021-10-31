ff134d03be63a39aa7b5d38916012c7c8f46c359
Felipe Aguilar <felipe@faguilar42.moti>
2021-10-31 01:42:12 -0300

    born2beroot

--- monitoring.sh
+++ monitoring.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-     ARCH=$(uname -a 2> /dev/null)
-      CPU=$(nproc 2> /dev/null)
-     vCPU=$(cat /proc/cpuinfo | grep processor | wc -l 2> /dev/null)
-MEM_USING=$(free | grep Mem: | awk '{print $3/1000}' | sed 's/\([0-9]\)\..*$/\1/g' 2> /dev/null)
-MEM_TOTAL=$(free | grep Mem: | awk '{print $2/1000}' | sed 's/\([0-9]\)\..*$/\1/g' 2> /dev/null)
-MEM_PCENT=$(free | grep Mem: | awk '{print $3/$2 * 100}' | sed 's/\(\.[0-9][0-9]\).*$/\1/g'2> /dev/null)
-DSK_USING=$(df --total -BG | awk 'END{print $3}' | sed 's/G//g' 2> /dev/null)
-DSK_TOTAL=$(df --total -BG | awk 'END{print $2}' 2> /dev/null)
-DSK_PCENT=$(df --total -BG | awk 'END{print $5}' 2> /dev/null)
- CPU_LOAD=$(top -n1 | grep %Cpu | awk '{print $2}' 2> /dev/null) 
-LAST_BOOT=$(who --boot | awk '{print $3 " " $4}' 2> /dev/null)
-LVM_COUNT=$(cat /etc/fstab | grep mapper | wc -l 2> /dev/null)
+     ARCH=$(uname -a)
+      CPU=$(nproc)
+     vCPU=$(cat /proc/cpuinfo | grep processor | wc -l)
+MEM_USING=$(free | grep Mem: | awk '{print $3/1000}' | sed 's/\([0-9]\)\..*$/\1/g')
+MEM_TOTAL=$(free | grep Mem: | awk '{print $2/1000}' | sed 's/\([0-9]\)\..*$/\1/g')
+MEM_PCENT=$(free | grep Mem: | awk '{print $3/$2 * 100}' | sed 's/\(\.[0-9][0-9]\).*$/\1/g')
+DSK_USING=$(df --total -BG | awk 'END{print $3}' | sed 's/G//g')
+DSK_TOTAL=$(df --total -BG | awk 'END{print $2}')
+DSK_PCENT=$(df --total -BG | awk 'END{print $5}')
+ CPU_LOAD=$(cat /proc/stat | grep cpu | head -1 | awk '{printf("%.1f%%", (($2 + $3 + $4) / $5 ) * 100) }')
+LAST_BOOT=$(who --boot | awk '{print $3 " " $4}')
+LVM_COUNT=$(cat /etc/fstab | grep mapper | wc -l)
   LVM_USE=no
 if [ $LVM_COUNT -gt 0 ]; then 
 	LVM_USE=yes
 fi
-TCP_CONNE=$(netstat -nat | grep ESTABLISHED | wc -l 2> /dev/null)
+TCP_CONNE=$(netstat -nat | grep ESTABLISHED | wc -l)
  USER_LOG=$(who --users | wc -l 2> /dev/null)
-  IP_ADDR=$(ip addr show enp0s3 | sed -n '3'p | awk '{print $2}' 2> /dev/null)
- MAC_ADDR=$(ip addr show enp0s3 | sed -n '2'p | awk '{print $2}' 2> /dev/null)
+  IP_ADDR=$(ip addr show enp0s3 | sed -n '3'p | awk '{print $2}')
+ MAC_ADDR=$(ip addr show enp0s3 | sed -n '2'p | awk '{print $2}')
 SUDO_COMM=$(($(journalctl _COMM=sudo | grep sudo | wc -l) / 3))
 
 wall "#Architecture	: ${ARCH} 
@@ -27,7 +27,7 @@ wall "#Architecture	: ${ARCH}
 #vCPU		: ${vCPU}
 #Memory Usage	: ${MEM_USING}/${MEM_TOTAL}MB (${MEM_PCENT}%)
 #Disk Usage	: ${DSK_USING}/${DSK_TOTAL}b (${DSK_PCENT})
-#CPU load	: ${CPU_LOAD}%
+#CPU load	: ${CPU_LOAD}
 #Last boot	: ${LAST_BOOT}
 #LVM use	: ${LVM_USE}
 #Connexions TCP	: ${TCP_CONNE} ESTABLISHED
